9a386dd821fa9469f1ed61bd25fabc9513a382b3
Nathan Madsen <nmadsen@familysearch.org>
2016-07-28 23:30:18 -0600

    playing with length converter

--- apprentice-length-converter-composition/src/main/java/nate/badge/App.java
+++ apprentice-length-converter-composition/src/main/java/nate/badge/App.java
@@ -6,6 +6,27 @@ package nate.badge;
 public class App {
   public static void main(String[] args) {
     //Reader reader = new Reader();
-    System.out.println("Hello World!");
+
+    System.out.println(feetToMeters(5280));
+    System.out.println(metersToFeet(400));
+    System.out.println(kilometersToMiles(100));
+    System.out.println(milesToKilometers(60));
+
+  }
+
+  private static double metersToFeet(double meters) {
+    return meters * 3.2808;
+  }
+
+  private static double feetToMeters(double feet) {
+    return feet / 3.2808;
+  }
+
+  private static double kilometersToMiles(double kilometers) {
+    return kilometers * 0.62137;
+  }
+
+  private static double milesToKilometers(double miles) {
+    return miles / 0.62137;
   }
 }
