5c2c9bfc3c94c9a9d62802504072a2f69c20ce48
Nathan Madsen <nmadsen@familysearch.org>
2016-08-01 08:29:20 -0600

    add another module for the parameters requirement

---
+++ apprentice-parameters/src/main/java/nate/badge/Person.java
@@ -0,0 +1,13 @@
+package nate.badge;
+
+/**
+ * Created by roondog on 8/1/16.
+ */
+public class Person {
+  String firstName;
+  String lastName;
+
+  public Person(String firstName, String lastName) {
+
+  }
+}
